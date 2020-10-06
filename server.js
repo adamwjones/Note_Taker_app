fb9dcdf3ceb9a245c6b5887b7e99397caf403b14
Adam Jones <adam.w.jones2@gmail.com>
2020-10-06 12:09:42 -0400

    cleaned comments and finalized code in server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function (req, res) {
-    //res.sendFile(path.join(__dirname, "./db/db.json"));
     fs.readFile("./db/db.json", "utf8", function (error, data) {
         if (error) {
             return console.log(error);
@@ -66,10 +65,8 @@ app.post("/api/notes", function(req, res) {
     // Each note needs a unique `id` when it's saved 
     newNote.id = uniqid();
     res.json(req.body);
-
     console.log(newNote);
 
-    // Add new note to the `db.json` file -> Is this all I have to do????
     fs.readFile('./db/db.json', "utf8", function(err, data) {
         if (err){
             return console.log(err);
@@ -83,7 +80,6 @@ app.post("/api/notes", function(req, res) {
             return true;
         });
     });
-    //res.send('DB updated');
 });
 
 app.get("*", function(req, res) {
@@ -95,87 +91,3 @@ app.get("*", function(req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
-
-
-
-
-
-
-
-
-
-// // Function to read JSON file
-// // =========================================================
-// jsonfile.readFile(file, function (err, obj) {
-//     if (err) console.error(err)
-//     console.log(obj)
-
-//     let notes = obj;
-
-//     // HTML Routes
-//     // =============================================================
-
-//     // GET `/notes` 
-//             // (Class Notes) GET Requests is when user or browser asks the webserver to get/give you something -> The GET Request triggers the code in the server route. The server then finds the relevant HTML content and data and responds by providing HTML 
-//         // Return the `notes.html` file
-
-//     app.get("/notes", function(req, res) {
-//         res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
-//     });
-
-//     // GET `*` 
-//             // (Class Notes) GET is when user or browser asks the webserver to get/give you something
-//         //Return the `index.html` file
-
-//     app.get("*", function(req, res) {
-//         res.sendFile(path.join(__dirname, "Develop/public/index.html"));
-//     });
-
-
-//     // API Routes // A direction to the webserver to do a certain thing
-//     // =============================================================
-
-//     // GET `/api/notes` 
-//             // (Class Notes) GET is when user or browser asks the webserver to get/give you something 
-
-//     app.get("/api/notes", function(req, res) {
-//         res.json(notes);
-//     });
-
-//         // Should read the `db.json` file
-//         // Return all saved notes as JSON
-
-
-//     // POST `/api/notes` 
-//             // (Class Notes) A POST Request is when the user sends something to the server and asks it to save it -> The POST Request triggers the code in the server route. The server then sets the data as "saved" in the DB -> Then the "Saved" data are loaded on the saved page 
-
-//     app.post("/api/notes", function(req, res) {
-//         // Receive a new note to save on the request body  
-//         let newNote = req.body;
-//         // Each note needs a unique `id` when it's saved 
-//         newNote.id = uniqid()
-//         console.log(newNote);
-//         // Add new note to the `db.json` file -> Is this all I have to do????
-//         notes.push(newNote);
-//         // Return the new note to the client
-//         res.json(newNote);
-//     });
-
-
-//     // DELETE `/api/notes/:id` 
-//             // (Class Notes) 
-//         // Should receive a query parameter containing the id of a note to delete 
-//             //(EAMPLE FROM ACTIVITIES) -> The /: lets you key off of unique character names 
-
-//                 // app.get("/:character", function(req, res) {
-//                 //     var chosen = req.params.character;
-//                 //     console.log(chosen);
-//                 //     res.end();
-//                 // });
-
-//         // Read all notes from the `db.json` file 
-
-//         // Remove the note with the given `id` property 
-
-//         // Rewrite the notes to the `db.json` file
-// });
